91861037729bb68bb6ab77243641247420551123
your_github_username <devenvaruv@gmail.com>
2024-05-19 16:53:17 -0700

    updated dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,27 @@ export default {
         sans: ['Montserrat', 'sans-serif'],
         heading: ['Lalezar', 'serif'],
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      minHeight: {
+        '20': '20px', // min-h-40
+      },
+      maxHeight: {
+        '40': '40px', // max-h-160
+      },
+      
     },
   },
   plugins: [],
